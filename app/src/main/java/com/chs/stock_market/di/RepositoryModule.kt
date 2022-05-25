91ef14856935304fee284d5dce2b1788e2983d32
hyeonseok <hs960829@gmail.com>
2022-05-25 23:19:11 +0900

    Apply in Hilt

---
+++ app/src/main/java/com/chs/stock_market/di/RepositoryModule.kt
@@ -0,0 +1,29 @@
+package com.chs.stock_market.di
+
+import com.chs.stock_market.data.csv.CSVParser
+import com.chs.stock_market.data.csv.CompanyListingsParser
+import com.chs.stock_market.data.repository.StockRepositoryImpl
+import com.chs.stock_market.domain.model.CompanyListing
+import com.chs.stock_market.domain.repository.StockRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindCompanyListingsParser(
+        companyListingsParser: CompanyListingsParser
+    ): CSVParser<CompanyListing>
+
+    @Binds
+    @Singleton
+    abstract fun bindStockRepository(
+        stockRepositoryImpl: StockRepositoryImpl
+    ): StockRepository
+}
---
+++ app/src/main/java/com/chs/stock_market/presentation/company_listings/CompanyListingsState.kt
@@ -0,0 +1,10 @@
+package com.chs.stock_market.presentation.company_listings
+
+import com.chs.stock_market.domain.model.CompanyListing
+
+data class CompanyListingsState(
+    val companies: List<CompanyListing> = emptyList(),
+    val isLoading: Boolean = false,
+    val isRefreshing: Boolean = false,
+    val searchQuery: String = ""
+)
